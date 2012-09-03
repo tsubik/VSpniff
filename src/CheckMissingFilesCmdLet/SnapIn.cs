dc4127a862986d5b069fd3bae9d07db2db553725
Tomasz Subik <tsubik@gmail.com>
2012-09-04 00:20:02 +0200

    initial commit

---
+++ src/CheckMissingFilesCmdLet/SnapIn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+using System.ComponentModel;
+
+namespace CheckVSMissingFilesCmdLet
+{
+    [RunInstaller(true)]
+    public class SnapIn : PSSnapIn
+    {
+        public override string Description
+        {
+            get { return "CheckmissingFiles"; }
+        }
+
+        public override string Name
+        {
+            get { return "CheckmissingFiles"; }
+        }
+
+        public override string Vendor
+        {
+            get { return "Tomasz Subik"; }
+        }
+    }
+}
