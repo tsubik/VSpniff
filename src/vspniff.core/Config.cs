46c6598439db976b4c2cf79fb252d062851ae7a5
tsubik <tsubik@gmail.com>
2013-04-13 16:15:39 +0200

    removing backwards compability, so what

--- src/vspniff.core/Config.cs
+++ src/vspniff.core/Config.cs
@@ -15,45 +15,7 @@ namespace VSpniff.Core
 
         public static Config Load(string fileName)
         {
-			string json = File.ReadAllText(fileName);
-			Config config = JsonConvert.DeserializeObject<Config>(json);
-			//just to backward compability
-			if (config == null)
-			{
-				config = ParseFileOldWay(fileName);
-			}
-
-			return config;
-		}
-		
-		private static Config ParseFileOldWay(string fileName)
-		{
-			Config config = new Config();
-			foreach (string line in File.ReadLines(fileName))
-			{
-				var workline = line.Trim().ToLower();
-
-				if (workline.StartsWith("mode:"))
-				{
-					string mode = workline.Replace("mode:", "");
-					if (mode == "override")
-					{
-						config.Mode = ConfigFileMode.Override;
-					}
-					else if (mode == "append")
-					{
-						config.Mode = ConfigFileMode.Append;
-					}
-				}
-				else if (workline.StartsWith("excludedextensions:"))
-				{
-					config.ExcludedExtensions = workline.Replace("excludedextensions:", "").Replace(" ","").Split(',');
-				}
-				else if (line.ToLower().StartsWith("excludeddirs:"))
-				{
-					config.ExcludedDirs = workline.Replace("excludeddirs:", "").Replace(" ","").Split(',');
-				}
-			}
+			Config config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(fileName));
 			return config;
 		}
 
